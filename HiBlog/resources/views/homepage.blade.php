115ddcb9a0237002e895e4d780424f633ccce01d
Leo <s1087262@studenti.univpm.it>
2022-07-21 12:53:38 +0200

    Homepage lv1 completata.
    In questo articolo vogliamo svelarvi alcuni trucchi e consigli per la caccia al cinghiale
    La veloce diffusione dell’animale ha portato il selvatico ad appropriarsi di nuovi ambiti territoriali e le modalità di caccia si stanno evolvendo per riuscire ad ottenere i migliori risultati.
    
    Vi consigliamo di leggere anche questo articolo sulle "abitudini e  caratteristiche del cinghiali"
    
    Qua sotto vogliamo elencarvi alcuni consgli e trucchi che vi potranno essere utili quando programmerete una battuta di caccia al cinghiale,

--- HiBlog/resources/views/homepage.blade.php
+++ HiBlog/resources/views/homepage.blade.php
@@ -51,9 +51,12 @@
             <div class="row d_flex">
                <div class="col-md-7">
                   <div class="titlepage">
-                     <h2>Informazioni sul sito e funzionalità</h2>
+                     <h2>Informazioni sull'azienda</h2>
                      <span></span>
-                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo </p>
+                     <p>Questo sito è stato realizzato dagli studenti del gruppo 53 del corso di Tecnologie Web di Ingegneria
+                      dell'informatica e dell'automazione, dell' Università Politecnica delle Marche. La documentaione 
+                      riguardo il progetto è disponibile <a href="" style="color:blue;">qui</a></p>
+                     
                      
                   </div>
                </div>
@@ -78,9 +81,15 @@
                </div>
                <div class="col-md-7">
                   <div class="titlepage">
-                     <h2>Informazioni sull'azienda</h2>
+                     <h2>Informazioni sul sito e funzionalità</h2>
                      <span></span>
-                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo </p>
+                     <p>HiBlog è un social network che permette alle persone di esprimere le proprie opinioni ed idee 
+                     riguardo tantissime tematiche. In questo sito è possibile registrarsi per poter poi accedere
+                     alle diverse funzionalità del sito quali: l'invio e la ricezione di richieste di amicizia dagli altri
+                     utenti, la creazione di blog o l'accesso ai blog dei propri amici per poter poi creare dei post 
+                     dove poter esprimere idee ed opinioni riguardo determinati argomenti, scelti a discrezione di chi
+                     crea il blog. </p>
+                     
                      
                   </div>
                </div>
@@ -96,13 +105,22 @@
                   <div class="titlepage">
                      <h2>Regolamenti del sito</h2>
                      <span></span>
-                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo </p>
+                     <p>Raccomandiamo gli utenti di rispettare il regolamento del sito, per evitare di perdere dei post 
+                         o direttamente i propri blog.</p> 
+                     <ul style="font-size:16px;">
+                         <li>- Rispettare tutti e non offendere nessuno.</li>
+                         <li>- Non creare post con contenuti che esprimano odio razziale, propaganda politica di qualsiasi
+                         tipo, violenza, bullismo, intimidazioni.</li>
+                         <li>- Non creare blog o post a fini commerciali. Per le inserzioni pubblicitarie contattate HiBlog
+                             direttamente tramite email. La potete trovare infondo alla pagina nella sezione contatti.</li>
+                     </ul>
+                     
                      
                   </div>
                </div>
                <div class="col-md-5">
                   <div class="about_img">
-                     <figure><img src="images/about_img.png" alt="#"/></figure>
+                     <figure><img style="max-height:596px;max-width:600px" src="images/regole.jpg" alt="#"/></figure>
                   </div>
                </div>
             </div>
@@ -115,15 +133,23 @@
             <div class="row d_flex">
                <div class="col-md-5">
                   <div class="mobile_img">
-                     <figure><img src="images/mobile.png" alt="#"/></figure>
+                     <figure><img style="max-height:596px;max-width:600px" src="images/hacker.jpg" alt="#"/></figure>
                   </div>
                </div>
                <div class="col-md-7">
                   <div class="titlepage">
                      <h2>Condizioni d'uso</h2>
                      <span></span>
-                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo </p>
+                     <p>Quando le opinioni e le azioni sono riconducibili alle persone, la nostra community è più sicura
+                         e più responsabile. Per questo motivo, l'utente è tenuto a:
                      
+                     usare lo stesso nome di cui si serve nella vita reale; fornire informazioni personali accurate; 
+                    creare un solo account (il proprio); non condividere la propria password, non concedere l'accesso 
+                    al proprio account HiBlog ad altri né trasferire il proprio account ad altri (senza l'autorizzazione
+                    di HiBlog, diffidandosi di chiunque altro).
+                    <br>
+                     Per qualsiasi problema o disguido contattare HiBlog servendosi delle informazioni di contatto
+                        presenti infondo alla pagina.</p>
                   </div>
                </div>
             </div>
